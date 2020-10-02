22e185620314bb1b699ec2a825372443701be8d3
waricoma <waricoma@devne.co>
2020-10-02 16:32:57 +0900

    add: express

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 'use strict';
 
-const StaticServer = require('static-server');
+const express = require('express');
+const app = express();
 
-const server = new StaticServer({
-  rootPath: '.',            // required, the root of the server file tree
-  port: 5000,               // required, the port to listen
-  name: 'http-server',   // optional, will set "X-Powered-by" HTTP header
-  host: '127.0.0.1',       // optional, defaults to any interface
-  cors: '*',                // optional, defaults to undefined
-  followSymlink: true,      // optional, defaults to a 404 error
-  templates: {
-    index: 'index.html',      // optional, defaults to 'index.html'
-    notFound: '404.html'    // optional, defaults to undefined
-  }
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname));
+
+app.use((req, res) => {
+  res.redirect(301, '/');
 });
 
-server.start(() => {
-  console.log('Server listening to', server.port);
+app.listen(app.get('port'), () => {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
